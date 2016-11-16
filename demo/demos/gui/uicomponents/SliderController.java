9e56a5d4ef335bbfe97160ea5d5e448524be7cd7
Kozinko Alexander <kozav@1c.ru>
2016-11-16 16:14:32 +0300

    OSGi-ed, Mavenified, removed android textfield and demo, removed gradle

--- demo/demos/gui/uicomponents/SliderController.java
+++
@@ -1,8 +0,0 @@
-package demos.gui.uicomponents;
-
-import io.datafx.controller.FXMLController;
-
-@FXMLController(value = "/resources/fxml/ui/Slider.fxml", title = "Material Design Example")
-public class SliderController {
-
-}
--- demo/demos/gui/uicomponents/SpinnerController.java
+++
@@ -1,8 +0,0 @@
-package demos.gui.uicomponents;
-
-import io.datafx.controller.FXMLController;
-
-@FXMLController(value = "/resources/fxml/ui/Spinner.fxml", title = "Material Design Example")
-public class SpinnerController {
-
-}
